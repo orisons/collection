9697c09ad8655bf5a8c76e44e19b4e6e9990d5d5
Krystian Drożdżyński <krystian@drozdzynski.me>
2017-08-04 13:50:23 +0200

    Initial :rocket:

---
+++ lib/collection.ex
@@ -0,0 +1,18 @@
+defmodule Orisons.Collection do
+  @moduledoc """
+  Documentation for Orisons.Collection.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Orisons.Collection.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/collection_test.exs
@@ -0,0 +1,8 @@
+defmodule Orisons.CollectionTest do
+  use ExUnit.Case
+  doctest Orisons.Collection
+
+  test "greets the world" do
+    assert Orisons.Collection.hello() == :world
+  end
+end
