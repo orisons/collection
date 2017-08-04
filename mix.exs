254ad1dc88030f47f9e125569a8ae98ab6b18faa
Krystian Drożdżyński <krystian@drozdzynski.me>
2017-08-04 14:12:11 +0200

    Prepare to release

--- mix.exs
+++ mix.exs
@@ -5,9 +5,11 @@ defmodule Orisons.Collection.Mixfile do
     [
       app: :collection,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.0",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Collection",
+      source_url: "https://github.com/orisons/collection"
     ]
   end
 
@@ -23,6 +25,23 @@ defmodule Orisons.Collection.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description do
+    """
+    Collection is a helper for working with data.
+    """
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :postgrex,
+      files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Krystian Drożdżyński", "Jakub Jasiulewicz"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/orisons/collection"}
     ]
   end
 end
