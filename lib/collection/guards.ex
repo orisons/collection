406c601463a3dd1d09caca57830668af432eb17d
Krystian Drożdżyński <krystian@drozdzynski.me>
2017-08-19 14:16:42 +0200

    Remove custom Guards

--- lib/collection/guards.ex
+++
@@ -1,9 +0,0 @@
-defmodule Orisons.Collection.Guards do
-    
-    defmacro is_list_map(list) do
-        quote do
-            Enum.all?(unquote(list), fn(x) -> is_map(x) end)
-        end
-    end
-
-end
